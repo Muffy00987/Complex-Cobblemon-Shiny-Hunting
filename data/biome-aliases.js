19a2e9a2a87bbfe2db25d313cf7c8d73d120072f
Muffy00987 <brandon.stuart20@yahoo.com>
2026-09-18 22:43:36 -0500

    Add Terralith biome aliases

---
+++ data/biome-aliases.js
@@ -0,0 +1,322 @@
+window.COBBLEMON_BIOME_ALIASES={
+  "terralith:ashen_savanna": [
+    "badlands",
+    "savanna"
+  ],
+  "terralith:red_oasis": [
+    "badlands",
+    "desert",
+    "savanna"
+  ],
+  "terralith:warped_mesa": [
+    "badlands",
+    "island"
+  ],
+  "terralith:white_mesa": [
+    "badlands"
+  ],
+  "terralith:sakura_grove": [
+    "cherry_blossom",
+    "floral",
+    "forest"
+  ],
+  "terralith:sakura_valley": [
+    "cherry_blossom",
+    "floral",
+    "forest",
+    "hills"
+  ],
+  "terralith:basalt_cliffs": [
+    "coast"
+  ],
+  "terralith:granite_cliffs": [
+    "coast"
+  ],
+  "terralith:white_cliffs": [
+    "coast"
+  ],
+  "terralith:cave/crystal_caves": [
+    "deep_dark"
+  ],
+  "terralith:cave/deep_caves": [
+    "deep_dark"
+  ],
+  "terralith:cave/frostfire_caves": [
+    "deep_dark",
+    "glacial"
+  ],
+  "terralith:cave/mantle_caves": [
+    "deep_dark",
+    "volcanic"
+  ],
+  "terralith:cave/tuff_caves": [
+    "deep_dark"
+  ],
+  "terralith:ancient_sands": [
+    "desert"
+  ],
+  "terralith:cave/desert_caves": [
+    "desert"
+  ],
+  "terralith:desert_canyon": [
+    "desert"
+  ],
+  "terralith:desert_oasis": [
+    "desert",
+    "savanna"
+  ],
+  "terralith:desert_spires": [
+    "desert"
+  ],
+  "terralith:lush_desert": [
+    "desert"
+  ],
+  "terralith:sandstone_valley": [
+    "desert"
+  ],
+  "terralith:fractured_savanna": [
+    "dripstone",
+    "savanna"
+  ],
+  "terralith:stony_spires": [
+    "dripstone",
+    "mountain"
+  ],
+  "terralith:blooming_plateau": [
+    "floral",
+    "highlands"
+  ],
+  "terralith:blooming_valley": [
+    "floral",
+    "forest",
+    "hills"
+  ],
+  "terralith:cloud_forest": [
+    "floral",
+    "taiga"
+  ],
+  "terralith:lavender_forest": [
+    "floral",
+    "forest"
+  ],
+  "terralith:lavender_valley": [
+    "floral",
+    "forest",
+    "hills"
+  ],
+  "terralith:alpha_islands": [
+    "forest",
+    "island"
+  ],
+  "terralith:alpha_islands_winter": [
+    "forest",
+    "island",
+    "snowy_forest"
+  ],
+  "terralith:forested_highlands": [
+    "forest",
+    "hills"
+  ],
+  "terralith:mirage_isles": [
+    "forest",
+    "island",
+    "magical",
+    "mushroom"
+  ],
+  "terralith:temperate_highlands": [
+    "forest",
+    "hills"
+  ],
+  "terralith:emerald_peaks": [
+    "freezing",
+    "peak",
+    "snowy"
+  ],
+  "terralith:scarlet_mountains": [
+    "freezing",
+    "peak",
+    "snowy"
+  ],
+  "terralith:skylands_winter": [
+    "freezing",
+    "sky",
+    "snowy"
+  ],
+  "terralith:snowy_badlands": [
+    "freezing",
+    "snowy",
+    "tundra"
+  ],
+  "terralith:frozen_cliffs": [
+    "frozen_ocean",
+    "glacial"
+  ],
+  "terralith:glacial_chasm": [
+    "glacial"
+  ],
+  "terralith:alpine_highlands": [
+    "highlands",
+    "shrubland"
+  ],
+  "terralith:arid_highlands": [
+    "highlands",
+    "savanna",
+    "shrubland"
+  ],
+  "terralith:highlands": [
+    "highlands"
+  ],
+  "terralith:lush_valley": [
+    "hills"
+  ],
+  "terralith:moonlight_valley": [
+    "hills",
+    "magical",
+    "taiga"
+  ],
+  "terralith:savanna_slopes": [
+    "hills",
+    "savanna"
+  ],
+  "terralith:yosemite_lowlands": [
+    "hills",
+    "taiga"
+  ],
+  "terralith:cave/underground_jungle": [
+    "jungle",
+    "lush"
+  ],
+  "terralith:amethyst_canyon": [
+    "magical"
+  ],
+  "terralith:amethyst_rainforest": [
+    "magical"
+  ],
+  "terralith:moonlight_grove": [
+    "magical",
+    "taiga"
+  ],
+  "terralith:volcanic_peaks": [
+    "mountain",
+    "volcanic"
+  ],
+  "terralith:windswept_spires": [
+    "mountain",
+    "peak"
+  ],
+  "terralith:yosemite_cliffs": [
+    "mountain"
+  ],
+  "terralith:cave/fungal_caves": [
+    "mushroom"
+  ],
+  "terralith:rocky_mountains": [
+    "peak"
+  ],
+  "terralith:brushland": [
+    "plains",
+    "savanna",
+    "shrubland"
+  ],
+  "terralith:scrubland": [
+    "plains"
+  ],
+  "terralith:steppe": [
+    "plains"
+  ],
+  "terralith:valley_clearing": [
+    "plains"
+  ],
+  "terralith:warm_river": [
+    "river"
+  ],
+  "terralith:hot_shrubland": [
+    "savanna",
+    "shrubland"
+  ],
+  "terralith:savanna_badlands": [
+    "savanna"
+  ],
+  "terralith:shrubland": [
+    "savanna",
+    "shrubland"
+  ],
+  "terralith:cold_shrubland": [
+    "shrubland",
+    "snowy_taiga",
+    "tundra"
+  ],
+  "terralith:rocky_shrubland": [
+    "shrubland",
+    "tundra"
+  ],
+  "terralith:skylands_autumn": [
+    "sky"
+  ],
+  "terralith:skylands_spring": [
+    "sky"
+  ],
+  "terralith:skylands_summer": [
+    "sky"
+  ],
+  "terralith:ice_marsh": [
+    "snowy_forest",
+    "swamp",
+    "taiga"
+  ],
+  "terralith:siberian_grove": [
+    "snowy_forest",
+    "snowy_taiga"
+  ],
+  "terralith:snowy_cherry_grove": [
+    "snowy_forest"
+  ],
+  "terralith:snowy_maple_forest": [
+    "snowy_forest",
+    "snowy_taiga"
+  ],
+  "terralith:alpine_grove": [
+    "snowy_taiga"
+  ],
+  "terralith:snowy_shield": [
+    "snowy_taiga"
+  ],
+  "terralith:wintry_forest": [
+    "snowy_taiga"
+  ],
+  "terralith:wintry_lowlands": [
+    "snowy_taiga"
+  ],
+  "terralith:orchid_swamp": [
+    "swamp"
+  ],
+  "terralith:bryce_canyon": [
+    "taiga"
+  ],
+  "terralith:haze_mountain": [
+    "taiga"
+  ],
+  "terralith:shield": [
+    "taiga"
+  ],
+  "terralith:siberian_taiga": [
+    "taiga"
+  ],
+  "terralith:yellowstone": [
+    "taiga",
+    "thermal",
+    "tundra"
+  ],
+  "terralith:caldera": [
+    "thermal"
+  ],
+  "terralith:cave/thermal_caves": [
+    "thermal"
+  ],
+  "terralith:gravel_desert": [
+    "tundra"
+  ],
+  "terralith:volcanic_crater": [
+    "volcanic"
+  ]
+};
